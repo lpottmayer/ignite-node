24a66496f870ca8f22495aa746db25a5edae0db5
Lucas Pottmayer <pottmayerlucas@gmail.com>
2021-03-25 14:30:09 -0300

    New features

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+
+class ImportCategoryUseCase {
+
+    execute(file: Express.Multer.File): void {
+        const stream = fs.createReadStream(file.path);
+
+        const parseFile = csvParse({
+            delimiter: ";"
+        });
+
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
+    }
+
+}
+
+export { ImportCategoryUseCase };
