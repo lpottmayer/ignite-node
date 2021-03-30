14746a0dfa5e3d996857da2ae9bcf96bebdc0f50
Lucas Pottmayer <pottmayerlucas@gmail.com>
2021-03-30 17:40:51 -0300

    User repository

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specification.routes';
+import { usersRoutes } from './users.routes';
 
 const router = Router();
 
 // Routes
+router.use("/users", usersRoutes);
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,16 @@
 import { container } from 'tsyringe';
+import { UsersRepository } from '../../modules/accounts/repositories/implementatios/UsersRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementatios/CategoriesRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementatios/SpecificationsRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
+);
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
